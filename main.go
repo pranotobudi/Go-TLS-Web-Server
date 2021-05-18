b1d8c5610786124c386c6cd3df28917f50d24b46
Pranoto Budi <pranotobudi.s@gmail.com>
2021-05-18 14:23:01 +0700

    TLS feature

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("bismillah")
+	go StartNonTLSServer()
+
+	mux := new(http.ServeMux)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	log.Println("Server started at :443")
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func StartNonTLSServer() {
+	mux := new(http.ServeMux)
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Redirecting to https://localhost/")
+		http.Redirect(w, r, "https://localhost/", http.StatusTemporaryRedirect)
+	}))
+
+	http.ListenAndServe(":80", mux)
 }
